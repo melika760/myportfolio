9c373323d41e484f1e01108288b25bb97392600e
melika760 <melikaaatafazoli@gmail.com>
2024-08-16 17:29:09 +0100

    adding icons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Styles from "./About.module.css"
 import me from "../assets/profile.png"
 import Js from "../assets/JS.png"
+import HTML from "../assets/HTML.png"
+import CSS from "../assets/CSS.png"
+import reacts from "../assets/react.png"
+import next from "../assets/Next.png"
 const About = () => {
   return (
     <section id='About' className={Styles.about}>
@@ -18,7 +22,30 @@ Fully committed to the philosophy of life-long learning, I’m a full stack deve
 <div className={Styles.skills}>
 <div className={Styles.rows}>
 <div className={Styles.skills__item}>
+    <img src={HTML} alt='HTML'/>
+    <div className={Styles.skillname}>HTML</div>
+</div>
+<div className={Styles.skills__item}>
+    <img src={CSS} alt='CSS'/>
+    <div className={Styles.skillname}>CSS</div>
+</div>
+<div className={Styles.skills__item}>
     <img src={Js} alt='JavaScript'/>
+    <div className={Styles.skillname}>JavaScript</div>
+</div>
+</div>
+<div className={Styles.rows}>
+<div className={Styles.skills__item}>
+    <img src={reacts} alt='React'/>
+    <div className={Styles.skillname}>React</div>
+</div>
+<div className={Styles.skills__item}>
+    <img src={next} alt='Next'/>
+    <div className={Styles.skillname}>Next.Js</div>
+</div>
+<div className={Styles.skills__item}>
+    <img src={Js} alt='JavaScript'/>
+    <div className={Styles.skillname}>JavaScript</div>
 </div>
 </div>
 </div>
