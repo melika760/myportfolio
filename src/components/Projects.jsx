ee088b790e9d8c6e76b53dd352d2e97e15e3bd94
melika760 <melikaaatafazoli@gmail.com>
2024-08-21 17:32:43 +0100

    adding projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import useCurrent from './useCurrent'
 import styles from "./Projects.module.css"
 import Dominique from "../assets/Dominiques.png"
+import Grocery from "../assets/GroceryStore.png"
+import Dictionary from "../assets/Dictionary.png"
 const Projects = () => {
   const{setsection}=useCurrent()
   useEffect(()=>{
@@ -11,8 +13,8 @@ const Projects = () => {
     <section id='Projects' className={styles.projects}>
       <h2 className={styles.head}>Projects</h2>
       <div className={styles.contents}>
-        <div className={styles.content}>
-          <a href='/' alt="Dominiques">
+        <div className={`${styles.content} ${styles.left}`}>
+          <a href='https://dominique-5aac4.web.app/' alt="Dominiques" target='__blank'>
           <div className={styles.pic}>
           <div className={styles.image}>
             <img src={Dominique} alt='DominiqueCafe'/>
@@ -22,10 +24,47 @@ const Projects = () => {
           
           <div className={styles.info}>
 <h3 className={styles.title}>Dominiques' Cafe</h3>
-<p className={styles.descriptions}>Full stack social media application built with React, Node.js, Express.js, MongoDB, Socket.io and AWS S3</p>
+<p className={styles.descriptions}>Full stack resturant website built with React,React-router,CSS Module,Firebase</p>
 
-<a href='/'><div className={styles.live1}>Live app</div></a>
-<a href='/'>Learn more</a>
+<a href='https://dominique-5aac4.web.app/' target='__blank' alt="Domique's Cafe"><div className={styles.live1}>Live app</div></a>
+<a href='/'><div className={styles.live1}>Learn more</div></a>
+
+
+          </div>
+        </div>
+        <div className={`${styles.content} ${styles.right}`}>
+          <div className={styles.info2}>
+<h3 className={styles.title}>Groccery Store</h3>
+<p className={styles.descriptions}>Full stack Groccery store  built with Nextjs,Tailwind CSS,Strapi</p>
+
+<a href='https://groceryshop-three.vercel.app/' target='__blank' alt="GroceryStore"><div className={styles.live1}>Live app</div></a>
+<a href='/'><div className={styles.live1}>Learn more</div></a>
+
+
+          </div>
+          <a href='https://groceryshop-three.vercel.app/' alt="GroceryStore" target='__blank'>
+          <div className={styles.pic2}>
+          <div className={styles.image2}>
+            <img src={Grocery} alt='GrocceryStore'/>
+          </div>
+          </div>
+          </a>
+        </div>
+<div className={`${styles.content} ${styles.left}`}>
+          <a href='https://musical-sorbet-7120c3.netlify.app/' alt="Dictionary" target='__blank'>
+          <div className={styles.pic}>
+          <div className={styles.image}>
+            <img src={Dictionary} alt='Dictionary'/>
+          </div>
+          </div>
+          </a>
+          
+          <div className={styles.info}>
+<h3 className={styles.title}>Dictionary App</h3>
+<p className={styles.descriptions}>Full stack Dictionary website built with React and Axios</p>
+
+<a href='https://musical-sorbet-7120c3.netlify.app/' target='__blank' alt="DictionaryApp"><div className={styles.live1}>Live app</div></a>
+<a href='/'><div className={styles.live1}>Learn more</div></a>
 
 
           </div>
